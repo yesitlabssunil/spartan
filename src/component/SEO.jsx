5495aaa20fa03183c9bfb533a362a87c1e412f9a
Sunil Rathore <yesitlabs.sunil@gmail.com>
2026-08-25 16:12:58 +0530

    add1

--- src/component/SEO.jsx
+++ src/component/SEO.jsx
@@ -58,7 +58,7 @@ const SEO = ({
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
-      <meta property="og:image" content="https://spartanlms.tgastaging.com/spartan_cms/uploads/1781779101_hero.svg" />
+      <meta property="og:image" content={image} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta property="og:image:alt" content={title} />
@@ -68,7 +68,11 @@ const SEO = ({
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={title} />
+      <meta name="twitter:image" content={image} />
+      <meta
+        name="twitter:image:alt"
+        content={title}
+      />
       <meta name="twitter:site" content="@spartan_handle" />
 
       {/* Global Schema */}
